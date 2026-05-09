5d5207400d3999fc68392aa404dec98a77d084ad
Eagleeliz <lizwanjiku573@gmail.com>
2026-05-09 17:31:45 +0300

    configuration updates

--- Backend/src/config.ts
+++ Backend/src/config.ts
@@ -1,4 +1,7 @@
 import dotenv from 'dotenv'
-dotenv.config({ path: './.env' })
+dotenv.config() 
 
 export const JWT_SECRET = process.env.JWT_SECRET as string;
+export const EMAIL_SENDER = process.env.EMAIL_SENDER as string;
+export const EMAIL_PASSWORD = process.env.EMAIL_PASSWORD as string;
+export const RESEND_API_KEY = process.env.RESEND_API_KEY as string;
